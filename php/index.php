d5333f3d7cd55680431a3555d475d15eb8b378dd
zoecutting <zoecutting@gmail.com>
2021-11-24 22:03:20 +0000

    "echo and print"

--- php/index.php
+++ php/index.php
@@ -1,15 +1,9 @@
 <?php
 
-echo 'zoe';
-
-$host = 'db';
-$user = 'devuser';
-$password = 'devpass';
-$db = 'test_db';
-
-$conn = new mysqli($host,$user,$password,$db);
-if($conn->connect_error){
-    echo  'connection failed' . $conn->connect_error;
-}
-echo 'successfully connected to msql';
-?>
+echo print 'Hello world';
+print '<br />';
+print 'Hello world';
+print '<br />';
+echo("Hello world");
+print '<br />';
+echo 'Hello', ' ', 'World';
