c70ad4d19fde8f4166ebea9c947fa531a01c9235
zoecutting <zoecutting@gmail.com>
2021-12-01 08:05:30 +0000

    return

--- php/return_declare_and_tickable_statements.php
+++ php/return_declare_and_tickable_statements.php
@@ -1,7 +1,7 @@
 <?php
 
 
-// RETURN / DECLARE / GOTO
+// RETURN / DECLARE
 // Using return without a value returns null
 
 echo sum(1, 2);
@@ -10,6 +10,32 @@ function sum(int $x, int $y) {
     return $x + $y;
 }
 
+// DECLARE - TICKS
+
+function onTick() {
+    echo 'Tick<br />';
+}
+
+register_tick_function('onTick');
+
+declare(ticks = 1);
+
+$i = 0;
+$length = 10;
+
+while ($i < $length) {
+    echo $i++ .  '<br />';
+}
+
+// DECLARE - ENCODING
+
+declare(strict_types=1);
+
+function sum(int $x, int $y) {
+    return $x + $y;
+}
+
+echo sum('5', 10);
 
 ?>
 
